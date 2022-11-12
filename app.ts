1df93820862ffb7d7e0f56c40d4a6f501583e8b8
ekim49 <hailieejkim@gmail.com>
2022-11-12 10:02:23 +0900

    added: union type

--- app.ts
+++ app.ts
@@ -1,51 +1,18 @@
-// const person: { name: string; age: number } = {
-// const person: {
-// 	name: string;
-// 	age: number;
-// 	hobbies: string[];
-// 	role: [number, string]; // tuple 타입
-// } = {
-// 	name: 'Harry',
-// 	age: 30,
-// 	hobbies: ['Sports', 'Cooking'],
-// 	role: [2, 'author'], // 길이는 2에 number, string 타입이 각각 값으로 와야 하는 타입이다.
-// };
-
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role { // 대문자로 시작하는 것이 컨벤션이다. 초기값을 지정하지 않으면 0부터 시작하는 것이 기본이고, 초기값을 설정한다면 그 숫자부터 1씩 증가해서 각각 할당된다. 각각의 값을 지정해주는 것도 가능하다. (숫자, 문자열 상관없이)
-	ADMIN, // 0
-	READ_ONLY, // 1
-	AUTHOR, // 2
+// ✨ union type
+// parameter에 여러 타입이 있어야 할 때 사용한다.
+function combine(input1: number | string, input2: number | string) {
+	let result;
+	// union type을 사용할 때, 작성하는 로직에 따라서 아래와 같이 런타임에서 if문으로 타입을 체크해야 할 때가 있다.
+	if (typeof input1 === 'number' && typeof input2 === 'number') {
+		result = input1 + input2; // numbers
+	} else {
+		result = input1.toString() + input2.toString(); // concatenate two strings
+	}
+	return result;
 }
 
-// ⚡️ any
-// any를 사용하면 모든 타입이 올 수 있다. 그러므로 타입스크립트의 이점을 누릴 수 없다.
-// 어떤 타입이 와야할지 불분명할 경우와 같은 불가피한 상황에만 사용하고 가급적 any를 사용하지 않는 것이 좋다.
-// any를 사용하게 될 경우에는 if문으로 런타임에서 타입을 체크할 수 있는 장치를 마련해야 한다.
+const combinedAges = combine(30, 26);
+console.log(combinedAges); // 56
 
-const person = {
-	name: 'Harry',
-	age: 30,
-	hobbies: ['Sports', 'Cooking'],
-	role: Role.ADMIN,
-};
-
-// person.role.push('admin');
-// person.role[1] = 10; // 타입스크립트는 role의 값으로 string 또는 number 타입이 와야한다는 사실만 알고 있어서 이 코드가 작동이 된다.
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-	// hobby는 string 이니까 string의 메서드를 적용할 수 있다. toUpperCase() 같은..
-	console.log(hobby.toUpperCase());
-}
-
-if (person.role === Role.AUTHOR) {
-	console.log('is author');
-}
+const combinedNames = combine('Max', 'Anna');
+console.log(combinedNames); // MaxAnna
--- basics.ts
+++ basics.ts
@@ -1,4 +1,7 @@
 function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+	// if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+	//   throw new Error('Incorrect input!');
+	// }
 	const result = n1 + n2;
 	if (showResult) {
 		// console.log(phrase + n1 + n2); // 이렇게 하면 phrase는 string, n1 + n2는 number 이니까 string + number 가 되면서 전체가 string 이 된다. => Result is: 52.8 이 출력됨. 그래서 2번째 줄 처럼 따로 n1 + n2의 결과값을 담은 변수를 선언한다.
