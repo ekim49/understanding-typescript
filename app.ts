b4d238c791f6b47d06ba50f445623b1859da6d9d
ekim49 <hailieejkim@gmail.com>
2022-11-15 17:59:18 +0900

    added: function return type and void

--- app.ts
+++ app.ts
@@ -1,46 +1,16 @@
-// ✨ type ailases
-// type 키워드로 시작하고, 대문자로 이름을 시작한다.
-// 복잡한 타입의 형태에 이름을 붙여 사용한다. 코드의 가독성 측면에서도 좋고, 오타로 에러가 발생할 일이 없어 유용하다.
-
-type Combinable = number | string; // 이렇게 union type일 경우 custom type을 만들면 유용하다.
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-// ✨ union type
-// parameter에 여러 타입이 있어야 할 때 사용한다.
-function combine(
-	// union type
-	// input1: number | string,
-	// input2: number | string,
-	// resultConversion: 'as-number' | 'as-text'
-
-	// type aliases
-	input1: Combinable,
-	input2: Combinable,
-	resultConversion: ConversionDescriptor
-) {
-	let result;
-	// union type을 사용할 때, 작성하는 로직에 따라서 아래와 같이 런타임에서 if문으로 타입을 체크해야 할 때가 있다.
-	if (
-		(typeof input1 === 'number' && typeof input2 === 'number') ||
-		resultConversion === 'as-number'
-	) {
-		result = +input1 + +input2; // numbers
-	} else {
-		result = input1.toString() + input2.toString(); // concatenate two strings
-	}
-	return result;
-	// if (resultConversion === 'as-number') {
-	// 	return +result;
-	// } else {
-	// 	return result.toString();
-	// }
+// ✨ function return type
+// 함수가 반환하는 값의 타입을 말한다.
+function add(n1: number, n2: number) {
+	return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges); // 56
+// ✨ void
+// 아무것도 반환하지 않는 함수일 때, 그 함수의 타입은 void 이다.
+function printResult(num: number): void {
+	console.log('Result: ' + num);
+}
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges); // 56
+printResult(add(5, 12)); // Result: 17
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames); // MaxAnna
+// let someValue: undefined; // 이렇게 하면 someValue는 영구적으로 undefined 타입을 갖게 된다.
+// undefined 는 return 이 필요하고, void는 return이 필요하지 않다. (void는 아무것도 반환하지 않을 때 사용하기 때문)
