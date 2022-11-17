4b2a971842721ca5cf3ce5f726a7c0e5ffdd86dc
ekim49 <hailieejkim@gmail.com>
2022-11-17 22:30:37 +0900

    added: function type

--- app.ts
+++ app.ts
@@ -12,5 +12,14 @@ function printResult(num: number): void {
 
 printResult(add(5, 12)); // Result: 17
 
+// let combineValues: Function; // 이렇게도 함수타입이라고 쓸 수 있기도 하지만, 아래처럼 더 구체적으로 쓸 수 있다.
+let combineValues: (a: number, b: number) => number; // 변수 combineValues는 함수 타입인데, 숫자 타입을 가진 두 개의 매개변수를 받아 숫자 타입의 값을 리턴하는 함수이다.
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8)); // 16
+
 // let someValue: undefined; // 이렇게 하면 someValue는 영구적으로 undefined 타입을 갖게 된다.
 // undefined 는 return 이 필요하고, void는 return이 필요하지 않다. (void는 아무것도 반환하지 않을 때 사용하기 때문)
