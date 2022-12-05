7f3707abbf4ec1d7ebe0f0bf1ef06d2b540b6365
ekim49 <hailieejkim@gmail.com>
2022-12-05 16:56:24 +0900

    added: never type

--- app.ts
+++ app.ts
@@ -9,3 +9,20 @@ userInput = 'Harry';
 if (typeof userInput === 'string') {
 	userName = userInput;
 }
+
+// never type
+// 절대로 아무것도 반환하지 않는 타입
+// 아래의 generateError 함수 예제와 같은 경우에도 never 타입을 사용할 수 있고,
+// infinite loop에도 사용할 수 있다.
+function generateError(message: string, code: number): never {
+	// 이 함수는 특정한 값을 절대로 반환하지 않는다. (= 아무것도 반환하지 않는다.)
+	// generateError 함수에 타입을 쓰기 전에 확인해보면 타입스크립트는 이 함수를 void로 타입 추론한다.
+	// 하지만 이 함수는 절대로 아무것도 반환하지 않는 함수이기에, never로 구체적으로 타입을 지정하면 명료하고 가독성있는 코드가 된다.
+	throw { message: message, errorCode: code };
+	// or
+	// while (true) {} -> infinite loop => never returns
+}
+
+// generateError('An error occurred!', 500);
+const result = generateError('An error occurred!', 500);
+console.log(result);
